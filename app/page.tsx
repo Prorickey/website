57d451c4c911d17246135d5cde75c0e0252b7d0d
Prorickey <trevobedson@gmail.com>
2024-04-30 08:46:53 -0400

    waves bs

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,29 @@
 'use client';
-import css from "./page.module.css";
+import Image from 'next/image';
+
+import './page.module.css'
+import waves from './waves.svg';
 
 export default function Home() {
   return (
     <main>
       <Introduction />
+      <Image
+        src={waves}
+        alt="Waves"
+        className="absolute w-full z-1 top-[24rem]"
+      />
     </main>
   );
 }
 
 function Introduction() {
   return (
-    <div className="w-full py-36 bg-gradient-to-br from-red-500 via-orange-600 to-amber-500 
+    <div className="w-full pt-36 pb-96 bg-gradient-to-b from-red-500 via-orange-600 to-amber-500 
     flex flex-col">
-      <p className="text-center pb-5 text-9xl font-sans font-bold tracking-wide">Trevor Bedson</p>
-      <p className="text-center pt-5">test</p>
+      <p className="inline-block text-center pb-5 text-9xl font-sans font-bold tracking-wide 
+      text-stone-900">Trevor Bedson</p>
+      <p className="text-center pt-5">"Banging stud"</p>
     </div>
   );
 }
