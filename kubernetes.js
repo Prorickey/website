8bdf2db9f132e18bec9fedd21d7273e2e7ddad14
Trevor Bedson <78997566+Prorickey@users.noreply.github.com>
2024-06-18 14:55:26 -0400

    Automatic kubernetes updating - now I need to add stricter authorization bc it's exposed to the world

---
+++ kubernetes.js
@@ -0,0 +1,50 @@
+const k8s = require("@kubernetes/client-node");
+const { version } = require("./package.json");
+const { config } = require("dotenv");
+config({ path: ".env.local" })
+
+const kc = new k8s.KubeConfig();
+kc.loadFromOptions({
+    clusters: [
+        {
+            name: "prorickey-cluster",
+            server: "https://kapi.prorickey.xyz",
+            skipTLSVerify: true,
+            caData: process.env.KUBECONFIGCERT
+        }
+    ],
+
+    users: [
+        {
+            name: "prorickey-portfolio-sa",
+            token: process.env.KUBECONFIGTOKEN
+        }
+    ],
+
+    contexts: [
+        {
+            name: "prorickey-context",
+            context: {
+                cluster: "prorickey-cluster",
+                user: "prorickey-portfolio-sa",
+                namespace: "prorickey"
+            }
+        }
+    ],
+
+    currentContext: "prorickey-context"
+})
+
+const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
+
+(async () => {
+    try {
+        const deployment = (await k8sApi.readNamespacedDeployment("portfolio", "prorickey")).body
+
+        deployment.spec.template.spec.containers[0].image = `docker.prorickey.xyz/prorickey/nextjs-portfolio:${version}`
+
+        await k8sApi.replaceNamespacedDeployment("portfolio", "prorickey", deployment)
+    } catch(err) {
+        console.error(err)
+    }
+})()
