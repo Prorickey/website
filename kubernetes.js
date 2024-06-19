909f4e83a7d0ee5c1ac181b89aec62ce9daf5202
Trevor Bedson <78997566+Prorickey@users.noreply.github.com>
2024-06-18 21:54:14 -0400

    version change and prettier

--- kubernetes.js
+++ kubernetes.js
@@ -1,51 +1,57 @@
 const k8s = require("@kubernetes/client-node");
 const { version } = require("./package.json");
 const { config } = require("dotenv");
-config({ path: ".env.local" })
+config({ path: ".env.local" });
 
 const kc = new k8s.KubeConfig();
 kc.loadFromOptions({
-    clusters: [
-        {
-            name: "prorickey-cluster",
-            server: "https://kapi.prorickey.xyz",
-            skipTLSVerify: true,
-            caData: process.env.KUBECONFIGCERT
-        }
-    ],
+  clusters: [
+    {
+      name: "prorickey-cluster",
+      server: "https://kapi.prorickey.xyz",
+      skipTLSVerify: true,
+      caData: process.env.KUBECONFIGCERT,
+    },
+  ],
 
-    users: [
-        {
-            name: "prorickey-portfolio-sa",
-            token: process.env.KUBECONFIGTOKEN
-        }
-    ],
+  users: [
+    {
+      name: "prorickey-portfolio-sa",
+      token: process.env.KUBECONFIGTOKEN,
+    },
+  ],
 
-    contexts: [
-        {
-            name: "prorickey-context",
-            context: {
-                cluster: "prorickey-cluster",
-                user: "prorickey-portfolio-sa",
-                namespace: "prorickey"
-            }
-        }
-    ],
+  contexts: [
+    {
+      name: "prorickey-context",
+      context: {
+        cluster: "prorickey-cluster",
+        user: "prorickey-portfolio-sa",
+        namespace: "prorickey",
+      },
+    },
+  ],
 
-    currentContext: "prorickey-context"
-})
+  currentContext: "prorickey-context",
+});
 
 const k8sApi = kc.makeApiClient(k8s.AppsV1Api);
 
 (async () => {
-    try {
-        const deployment = (await k8sApi.readNamespacedDeployment("portfolio", "prorickey")).body
+  try {
+    const deployment = (
+      await k8sApi.readNamespacedDeployment("portfolio", "prorickey")
+    ).body;
 
-        deployment.spec.template.spec.containers[0].image = `docker.prorickey.xyz/prorickey/nextjs-portfolio:${version}`
-        deployment.spec.replicas = 3
+    deployment.spec.template.spec.containers[0].image = `docker.prorickey.xyz/prorickey/nextjs-portfolio:${version}`;
+    deployment.spec.replicas = 3;
 
-        await k8sApi.replaceNamespacedDeployment("portfolio", "prorickey", deployment)
-    } catch(err) {
-        console.error(err)
-    }
-})()
+    await k8sApi.replaceNamespacedDeployment(
+      "portfolio",
+      "prorickey",
+      deployment,
+    );
+  } catch (err) {
+    console.error(err);
+  }
+})();
